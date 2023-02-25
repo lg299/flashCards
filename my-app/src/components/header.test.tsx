add491266f08c90e667c9b6804fdcedb919d958e
lg299 <lizzgriffin@hotmail.co.uk>
2023-02-25 14:28:07 +0000

    Added header with test

---
+++ my-app/src/components/header.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from './header'
+
+
+describe("<Header />", () => {
+    const result = render(< Header/>);
+    const header = result.container.querySelector('#header-time')
+
+    test("return header", () => {
+        expect(header).toHaveTextContent("flashCards")
+    })
+
+});
---
+++ my-app/src/components/header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(){
+    return (
+    <div id="header-time">
+        <p id="title">flashCards</p>
+    </div>
+    );
+}
+
+export default Header;
