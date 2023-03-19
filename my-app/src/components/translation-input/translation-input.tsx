f0df98543e1f99a6fdcee0fb155e16d33f357678
lg299 <lizzgriffin@hotmail.co.uk>
2023-03-19 12:43:39 +0000

    Removed styling from css file and added to makestyles object

--- my-app/src/components/translation-input/translation-input.tsx
+++ my-app/src/components/translation-input/translation-input.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 import { makeStyles } from '@mui/styles';
-import './translation-input.css'
+import Paper from '@mui/material/Paper';
 
 
 const useStyles = makeStyles({
     textField: {
-        marginLeft: 'auto',
-        marginRight: 'auto',
+        margin: '0 auto',
         paddingBottom: 0,
         marginTop: 0,
+        '& .MuiFormLabel-root' : {
+            color: 'white',
+            '&.Mui-focused' : {
+                color: 'white',
+                fontWeight: 'bold',
+            },
+        },
+        '& .MuiInput-root:before':{
+            borderBottom: '2px solid white',
+            color: 'white',
+        },
+        '& .MuiInput-root:after':{
+            borderBottom: '2px solid white',
+            color: 'white',
+        },
+        '& .MuiInput-input': {
+            borderBottom: '2px solid white',
+            color: 'white',
+        },
+        '& .MuiInput-underline:after': {
+            color: 'white',
+        },
+        '& .MuiInput-root:hover:not(.Mui-disabled, .Mui-error):before' :{
+            border: '0',
+        },
+    },
+    paper: {
+        margin: "2rem 45rem", 
+        padding: "1rem 0", 
+        // backgroundColor: "rgb(188, 112, 163)",
     }
 });
 
@@ -18,15 +47,14 @@ function TranslationInput() {
 
     return (
         <div>
-            <TextField
-                id="standard"
-                label="Input translation"
-                variant="standard"
-                className={styles.textField}
-
-            // needs to be
-            // InputProps= {{ style : {color: 'white'}}}
-            />
+            <Paper elevation={3} className={styles.paper} sx={{backgroundColor: 'rgb(188, 112, 163)'}}>
+                <TextField
+                    id="standard"
+                    label="Input translation"
+                    variant="standard"
+                    className={styles.textField}
+                />
+            </Paper>
         </div>
     )
 }
