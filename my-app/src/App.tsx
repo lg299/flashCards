21d34ff04376dd4aeca1a048e9e6e96ce7b50c6c
lg299 <lizzgriffin@hotmail.co.uk>
2023-03-21 10:48:38 +0000

    Converted styles to makeStyles and reformatted

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,8 +4,18 @@ import Header from './components/header/header'
 import TranslationInput from './components/translation-input/translation-input';
 import Grid from '@mui/material/Grid';
 import ArrowRightAltOutlinedIcon from '@mui/icons-material/ArrowRightAltOutlined';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  arrow: {
+    color: 'white',
+    paddingTop: "1rem"
+  }
+})
 
 function App() {
+  const styles = useStyles()
+
   return (
     <div className="App">
       <div>
@@ -15,13 +25,19 @@ function App() {
         <Grid container spacing={2}>
           <Grid item xs={3}></Grid>
           <Grid item xs={2}>
-            <TranslationInput label='Quiero una pizza' disabled={true} />
+            <TranslationInput
+              label='Quiero una pizza'
+              disabled={true}
+            />
           </Grid>
           <Grid item xs={2}>
-            <ArrowRightAltOutlinedIcon style={{ color: 'white', paddingTop: '1rem' }} />
+            <ArrowRightAltOutlinedIcon className={styles.arrow} />
           </Grid>
           <Grid item xs={2}>
-            <TranslationInput label='Input Translation' disabled={false} />
+            <TranslationInput
+              label='Input Translation'
+              disabled={false}
+            />
           </Grid>
           <Grid item xs={3}></Grid>
         </Grid>
