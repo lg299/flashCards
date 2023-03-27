a078cc1710d3c98c976ec46a61e3c330be8e2689
lg299 <lizzgriffin@hotmail.co.uk>
2023-03-27 11:36:29 +0100

    WIP: adding styled version of component

--- my-app/src/components/translation-input/translation-input.tsx
+++ my-app/src/components/translation-input/translation-input.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import TextField from '@mui/material/TextField';
 import { makeStyles } from '@mui/styles';
 import Paper from '@mui/material/Paper';
+import { alpha, styled } from '@mui/material/styles';
 
 
 const useStyles = makeStyles({
@@ -9,58 +10,71 @@ const useStyles = makeStyles({
         margin: '0 auto',
         paddingBottom: 0,
         marginTop: 0,
-        '& .MuiFormLabel-root' : {
-            color: 'white',
-            '&.Mui-focused' : {
-                color: 'white',
-                fontWeight: 'bold',
-            },
-        },
-        '& .MuiInput-root:before':{
-            borderBottom: '2px solid white',
-            color: 'white',
-        },
-        '& .MuiInput-root:after':{
-            borderBottom: '2px solid white',
-            color: 'white',
-        },
-        '& .MuiInput-input': {
-            borderBottom: '2px solid white',
-            color: 'white',
-        },
-        '& .MuiInput-underline:after': {
-            color: 'white',
-        },
-        '& .MuiInput-root:hover:not(.Mui-disabled, .Mui-error):before' :{
-            border: '0',
-        },
     },
     paper: {
-        margin: "2rem 45rem", 
-        padding: "1rem 0", 
+        margin: "2rem 45rem",
+        padding: "1rem 0",
         // backgroundColor: "rgb(188, 112, 163)",
     }
 });
 
-interface InputProps {
-    label: string 
+const borderStyling = {
+    borderBottom: '2px solid white',
+    color: 'white',
 }
 
-function TranslationInput(props: InputProps) {
-    const styles = useStyles();
+const StyledTextFeild = styled(TextField)<TextFieldProps>(({ theme }) => ({
+    width: 300,
+    color: theme.palette.success.main,
+    '& .MuiFormLabel-root': {
+        color: 'white',
+        '&.Mui-focused': {
+            color: 'white',
+            fontWeight: 'bold',
+        },
+    },
+    '& .MuiInput-root:before': borderStyling,
+    '& .MuiInput-root:after': borderStyling,
+    '& .MuiInput-input': borderStyling,
+    '& .MuiInput-underline:after': {
+        color: 'white',
+    },
+    '& .MuiInput-root:hover:not(.Mui-disabled, .Mui-error):before': {
+        border: '0',
+    },
+}));
 
+interface TextFieldProps {
+    label: string
+}
+
+// function TranslationInput(props: TextFieldProps) {
+//     const styles = useStyles();
+
+//     return (
+//         <div>
+//             <Paper elevation={3} className={styles.paper} sx={{ backgroundColor: 'rgb(188, 112, 163)' }}>
+//                 <TextField
+//                     id="standard"
+//                     label={props.label}
+//                     variant="standard"
+//                     className={styles.textField}
+//                 />
+//             </Paper>
+//         </div>
+//     )
+// }
+
+const TranslationInput: React.FC<TextFieldProps> = (props) => {
+    const { label } = props;
     return (
-        <div>
-            <Paper elevation={3} className={styles.paper} sx={{backgroundColor: 'rgb(188, 112, 163)'}}>
-                <TextField
-                    id="standard"
-                    label={props.label}
-                    variant="standard"
-                    className={styles.textField}
-                />
-            </Paper>
-        </div>
-    )
-}
+        <StyledTextFeild
+            id="standard"
+            label={props.label}
+            variant="standard"
+        />
+    );
+};
+
 
 export default TranslationInput;
